b49000266ecb05b071085515a2aed4a4fc0eee7f
KN <kn@kn.com>
2025-10-02 16:18:57 +0800

    0.0.0.0

--- app_q.py
+++ app_q.py
@@ -118,7 +118,7 @@ def index():
                            result=result)
 
 if __name__ == "__main__":
-    host = os.environ.get("HOST", "127.0.0.1")
+    host = os.environ.get("HOST", "0.0.0.0")
     port = int(os.environ.get("PORT", "5000"))
     debug = os.environ.get("DEBUG", "0") in ["1", "true", "True"]
     app.run(host=host, port=port, debug=debug)
